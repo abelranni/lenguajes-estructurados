cd0d93a3d315e57e84d4f7e96153cf649e957c84
abel.ranni <abel.ranni@gmail.com>
2022-04-19 20:47:09 -0300

    ajustes

--- Proyectos/8-algoritmos/8-3.c
+++ Proyectos/8-algoritmos/8-3.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <windows.h>
+
 int main()
 {
     unsigned int i, j, x;
@@ -7,22 +7,23 @@ int main()
     printf("Introduce el valor de x: ");
     scanf("%d", &x);
 
-    // Calcula los primero 10 valores para e elevado a x.
+    // Calcula los primeros 20 valores para e elevado a x.
     e = 1.0;
-    for (i = 1; i <= 30; i++)
+    for (i = 1; i <= 20; i++)
     {
         // Calcula x elevada a una potencia i
         potencia = 1;
         for (j = 1; j <= i; j++)
             potencia = potencia * x;
+
         // Calcula el Factorial de i
         factorial = 1;
         for (j = i; j > 0; j--)
             factorial = factorial * j;
-        // Acumula el valor de e
 
+        // Acumula el valor de e
         e = e + (potencia / factorial);
-        printf("e : %f ", e);
+
     }
 
     printf("El valor de e elevado a x: %f", e);
